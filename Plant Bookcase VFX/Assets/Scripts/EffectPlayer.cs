57cae34b8f0270f68a5c8a55239cd08d490f6b78
Evan Li <eli24@ucsc.edu>
2021-11-27 13:56:57 -0800

    MultiEffectPlayer, play button, burst charge vfx, timing & color changes

---
+++ Plant Bookcase VFX/Assets/Scripts/EffectPlayer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.Events;
+
+public class EffectPlayer : MonoBehaviour
+{
+    [SerializeField] private VisualEffect effect;
+
+    [SerializeField] bool loop;
+    [SerializeField] float startDelay;
+    [SerializeField] float endDelay;
+
+    private UnityEvent onEffectPlay = new UnityEvent();
+
+    public void Construct()
+    {
+        if (effect == null)
+        {
+            Debug.LogError(this + " Error: effect cannot be null");
+        }
+
+        if (loop)
+        {
+            onEffectPlay.AddListener(StartLoopEffect);
+        }
+        else
+        {
+            onEffectPlay.AddListener(StartPlayEffect);
+        }
+    }
+
+    public void StartEffectPlayer()
+    {
+        onEffectPlay.Invoke();
+    }
+
+    private void StartLoopEffect()
+    {
+        StartCoroutine(LoopEffect());
+    }
+
+    private void StartPlayEffect()
+    {
+        StartCoroutine(PlayEffect());
+    }
+
+    private IEnumerator LoopEffect()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(startDelay);
+            effect.Play();
+            yield return new WaitForSeconds(endDelay);
+        }
+    }
+
+    private IEnumerator PlayEffect()
+    {
+        yield return new WaitForSeconds(startDelay);
+        effect.Play();
+    }
+}
---
+++ Plant Bookcase VFX/Assets/Scripts/MultiEffectPlayer.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.Events;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+public class MultiEffectPlayer : MonoBehaviour
+{
+    [SerializeField] private List<EffectPlayer> effectPlayerList = new List<EffectPlayer>();
+    [SerializeField] private List<Animator> animatorList = new List<Animator>();
+    [SerializeField] private List<PlayableDirector> playableDirectorList = new List<PlayableDirector>();
+
+    [SerializeField] private GameObject playButtonObject;
+    [SerializeField] private float effectDuration;
+    [SerializeField] private bool loop;
+
+    private void Awake()
+    {
+        foreach (EffectPlayer effect in effectPlayerList)
+        {
+            effect.Construct();
+        }
+        foreach (Animator animator in animatorList)
+        {
+            animator.enabled = false;
+        }
+    }
+
+    public void StartPlayEffects()
+    {
+        StartCoroutine(PlayEffects());
+    }
+
+    private IEnumerator PlayEffects()
+    {
+        playButtonObject.SetActive(false);
+
+        foreach (EffectPlayer effect in effectPlayerList)
+        {
+            effect.StartEffectPlayer();
+        }
+        foreach (Animator animator in animatorList)
+        {
+            animator.enabled = true;
+            animator.Play("Base Layer.Effect", -1, 0f);
+        }
+        foreach (PlayableDirector playable in playableDirectorList)
+        {
+            playable.Play();
+        }
+
+        yield return new WaitForSeconds(effectDuration);
+
+        if(loop)
+        {
+            StartCoroutine(PlayEffects());
+        }
+        else
+        {
+            playButtonObject.SetActive(true);
+        }
+    }
+}
